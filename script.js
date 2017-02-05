8d24a23538a82eb04dcad5c7f6c7c6e813296ffb
Katherine <katgm10@gmail.com>
2017-02-04 19:26:09 -0800

    f

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var i = 0;
+var i = 1;
 
 function duplicate() {
-	var original = document.getElementById("duplicater");
+	i = i+1;
+	var original = document.getElementById("button");
     var clone = original.cloneNode(true); // "deep" clone
-    clone.id = "duplicater" + ++i;
+    clone.id = "button" + i;
     // or clone.id = ""; if the divs don't need an ID
-    document.getElementById("body").parentNode.appendChild(clone);
+    original.parentNode.appendChild(clone);
 }
