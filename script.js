e36f7f2d5d93f32269dbef1fdb7a02c3bd8d487a
Katherine <katgm10@gmail.com>
2017-02-04 20:36:32 -0800

    jasfh

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ function duplicate() {
 	var original = document.getElementById("question");
     var clone = original.cloneNode(true); // "deep" clone
     clone.id = "question" + i;
+    for(b = 0; b > 5; b++){
+    	clone.children[b].id = original.children.id[b] + i;
+    }
     // or clone.id = ""; if the divs don't need an ID
     original.appendChild(clone);
 }
 
 function startGame(){
 	for(a = 0; a < i; a++){
-		var questionString = document.getElementById("question"+a).getElementById("qs").val();
+		var questionString = document.getElementById("question"+a);
 		console.log(questionString);
 	}
 }
