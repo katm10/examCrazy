a6a4dbc9204d78e0f058fa95edf2fd7085784009
18PatZ <18patz@students.harker.org>
2017-02-04 19:28:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var i = 1;
 
 function duplicate() {
 	i = i+1;
+	console.log(i);
 	var original = document.getElementById("button");
     var clone = original.cloneNode(true); // "deep" clone
     clone.id = "button" + i;
