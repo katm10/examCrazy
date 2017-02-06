764aac6c00a429b4a258919ba2137639720d299d
18PatZ <18patz@students.harker.org>
2017-02-06 10:40:17 -0800

    Create firebase_initialization.js

---
+++ firebase_initialization.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyB0YLiSfVf8aDluRvk268eCL_mpZFXYug0",
+	authDomain: "exam-crazy.firebaseapp.com",
+	databaseURL: "https://exam-crazy.firebaseio.com",
+	storageBucket: "exam-crazy.appspot.com",
+	messagingSenderId: "788284782428"
+};
+
+firebase.initializeApp(config);
