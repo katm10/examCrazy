6781dc8046b31c048433caa966070def57ee1894
Katherine <katgm10@gmail.com>
2017-02-05 17:28:19 -0800

    update

--- script.js
+++ script.js
@@ -21,13 +21,14 @@ function duplicate() {
 function startGame(){
 	for(a = 0; a < i; a++){
 		var currentQuestionBlock = document.getElementById("question"+a);
-		var questionStr =  $('#'+'questionStr-'+a).text(); 
-		/*var falseAnswer1 = currentQuestionBlock.children[1].val();
-		var falseAnswer2 = currentQuestionBlock.children[2].val();
-		var falseAnswer3 = currentQuestionBlock.children[3].val();
-		var correctAnswer = currentQuestionBlock.children[4].val();
-*/
-console.log("question " + a+ " is " + questionStr);
+		var questionStr =  $('#'+'questionStr-'+a).val(); 
+		var falseAnswer1 =  $('#'+'falseAnswer1-'+a).val();
+		var falseAnswer2 =  $('#'+'falseAnswer2-'+a).val();
+		var falseAnswer3 =  $('#'+'falseAnswer3-'+a).val();
+		var correctAnswer =  $('#'+'correctAnswer-'+a).val();
+
+		console.log(questionString + " " + falseAnswer1 + " " + falseAnswer2 + " " + falseAnswer3 + " " + correctAnswer);
+
 	}
   /*for(a = 0; a < i; a++){
   	var questionString = document.getElementById("questionStr-"+a).val();
