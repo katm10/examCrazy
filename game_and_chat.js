1586e0373ccd92561fca645f143f341a567d2972
18PatZ <18patz@students.harker.org>
2017-02-06 10:41:52 -0800

    Remove duplicate initialization

--- game_and_chat.js
+++ game_and_chat.js
@@ -1,15 +1,6 @@
 
 window.onload = function(){
 
-	var config = {
-		apiKey: "AIzaSyB0YLiSfVf8aDluRvk268eCL_mpZFXYug0",
-		authDomain: "exam-crazy.firebaseapp.com",
-		databaseURL: "https://exam-crazy.firebaseio.com",
-		storageBucket: "exam-crazy.appspot.com",
-		messagingSenderId: "788284782428"
-	};
-	firebase.initializeApp(config);
-
 	var url = window.location.href;
 	var index = url.indexOf("?");
 	if(index < 0)
