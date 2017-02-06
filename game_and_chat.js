0af0870407d10587a1a5f193687ae929e9d5e92a
18PatZ <18patz@students.harker.org>
2017-02-06 10:38:17 -0800

    Firebase initialization, checking if chatroom exists

--- game_and_chat.js
+++ game_and_chat.js
@@ -1,9 +1,28 @@
 
 window.onload = function(){
 
-  var url = window.location.href;
-  var index = url.indexOf("?");
-  var chatroomID = url.substring(index, url.length);
-  console.log(chatroomID);
+	var config = {
+		apiKey: "AIzaSyB0YLiSfVf8aDluRvk268eCL_mpZFXYug0",
+		authDomain: "exam-crazy.firebaseapp.com",
+		databaseURL: "https://exam-crazy.firebaseio.com",
+		storageBucket: "exam-crazy.appspot.com",
+		messagingSenderId: "788284782428"
+	};
+	firebase.initializeApp(config);
+
+	var url = window.location.href;
+	var index = url.indexOf("?");
+	if(index < 0)
+		return;
+	var chatroomID = url.substring(index + 1, url.length);
+	console.log(chatroomID);
+	
+	var chatroomRef = firebase.database().ref('chatrooms/'+chatroomID);
+
+	firebase.database().ref('chatrooms/'+chatroomID).once('value').then(function(snapshot) {
+		if(snapshot.val() == null){
+				alert("This chatroom does not exist.");
+		}
+	});
 
 };
