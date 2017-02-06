f0d78cec016c1c2c86512bed05e1d12c82a5cf6e
18PatZ <18patz@students.harker.org>
2017-02-05 18:03:53 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function keyUp(event){
+	var x = event.which || event.keyCode;
+	if(x == 13){
+		var entered = document.getElementById("privateCode").value;
+		if(entered.length > 0){
+			messagesRef = firebase.database().ref('chatrooms/'+entered);
+
+			firebase.database().ref('chatrooms/'+entered).once('value').then(function(snapshot) {
+				if(snapshot.val() == null){
+					if(entered == "1"){
+						firebase.database().ref('chatrooms/1').set({
+							messages: [""],
+							users: [""]
+						});
+						alert("Placeholder for making chat");
+					}
+					else {
+						alert("Incorrect chat PIN.");
+					}
+				}
+				else {
+					alert("Placeholder for joining.");
+				}
+			});
+		}
+	}
+}
