401d18a21e0b75d556c00ccdc8c4c6fc86a1cc2c
Patrick Zhong <18PatZ@students.harker.org>
2017-02-07 10:41:48 -0800

    Moar options

--- index.js
+++ index.js
@@ -7,31 +7,34 @@ window.onload = function(){
 function keyUp(event){
 	var x = event.which || event.keyCode;
 	if(x == 13){
-		var entered = document.getElementById("privateCode").value;
-		if(entered.length > 0){
-			messagesRef = firebase.database().ref('chatrooms/'+entered);
-
-			firebase.database().ref('chatrooms/'+entered).once('value').then(function(snapshot) {
-				if(snapshot.val() == null){
-					if(entered == "1"){
-						firebase.database().ref('chatrooms/1').set({
-							messages: [""],
-							users: [""]
-						});
-						alert("Placeholder for making chat");
-					}
-					else {
-						document.getElementById("warning").innerHTML = "Code not recognized";
-					}
-				}
-				else {
-					window.location = "https://katm10.github.io/examCrazy/game_and_chat.html?"+entered;
-				}
-			});
-		}
-		
+		submit();
 		return true;
 	}
 	
 	return false;
+}
+
+function submit(){
+	var entered = document.getElementById("privateCode").value;
+	if(entered.length > 0){
+		messagesRef = firebase.database().ref('chatrooms/'+entered);
+
+		firebase.database().ref('chatrooms/'+entered).once('value').then(function(snapshot) {
+			if(snapshot.val() == null){
+				if(entered == "1"){
+					firebase.database().ref('chatrooms/1').set({
+						messages: [""],
+						users: [""]
+					});
+					alert("Placeholder for making chat");
+				}
+				else {
+					document.getElementById("warning").innerHTML = "Code not recognized";
+				}
+			}
+			else {
+				window.location = "https://katm10.github.io/examCrazy/game_and_chat.html?chatroomNum="+entered;
+			}
+		});
+	}
 }
