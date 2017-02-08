9fce28ecc7efd2adf54cee434bd931a648bd4454
Patrick Zhong <18PatZ@students.harker.org>
2017-02-08 12:59:07 -0800

    Same widths

--- join_public.js
+++ join_public.js
@@ -2,12 +2,27 @@ window.onload = function(){
 	
 	var messagesRef = firebase.database().ref('chatrooms/public/').orderByKey();
 
+	var max = 0;
+	var size = 0;
 	messagesRef.once('value').then(function(snapshot) {
 		snapshot.forEach(function(childSnapshot){
 			if(childSnapshot.val() != null){
-				document.getElementById("roomList").innerHTML += '<a class="btn btn-lg btn-success" style="width:250px" onclick = "send(this)">'+childSnapshot.key+'</a><br><br>';
+				document.getElementById("roomList").innerHTML += '<a id="e'+size+'" class="btn btn-lg btn-success" onclick = "send(this)">'+childSnapshot.key+'</a><br><br>';
+				size++;
 			}
 		});
+		
+		for(var i = 0; i < size; i++){
+			var width = document.getElementById("e"+i).clientWidth;
+			
+			if(width > max){
+				max = width;
+			}
+		}
+
+		for(var i = 0; i < size; i++){
+			document.getElementById("e"+i).style.width = max;
+		}	
 	});
 }
 
